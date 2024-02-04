af13ecfa4f4bdf499a514cbab7de74cd952df0a9
rampremad <146694049+rampremad@users.noreply.github.com>
2024-02-04 12:46:19 +0530

    new

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,31 @@
 function claimUrl() {
-    var username = $('#usernameInput').val();
-    $.ajax({
-        url: 'https://app.khojorightnow.com/api/v1/api-public/username/' + username,
-        method: 'GET',
-        success: function (response) {
-            console.log(response);
-            if (response.success) {
-                showStatusMessage('error', 'That username is already taken');
-            } else {
-                showStatusMessage('success', 'Yay!🎉 URL available, Claim now!');
-            }
-        },
-        error: function (error) {
-            console.error('API request failed:', error);
-            showStatusMessage('error', 'Claim failed. Please try again.');
-        }
+    $(document).ready(function() {
+        $('#statusContainer').hide();
     });
-}
+     var username = $('#usernameInput').val();
+     $.ajax({
+         url: 'https://app.khojorightnow.com/api/v1/api-public/username/' + username,
+         method: 'GET',
+         success: function(response) {
+             console.log(response.status);
+             if (!response.status) {
+                 showStatusMessage('error', 'That username is already taken');
+                 showStatusMessage('success', '');
+             } else {
+                 showStatusMessage('success', 'Yay!🎉 URL available, Claim now!');
+                 showStatusMessage('error', '');
+             }
+         },
+         error: function(error) {
+             console.error('API request failed:', error);
+             showStatusMessage('error', 'Claim failed. Please try again.');
+             showStatusMessage('success', '');
+         }
+     });
+ }
 
-function showStatusMessage(type, message) {
-    $('#statusContainer').show();
-
-    var statusElement = $('#statusContainer h6.' + type);
-    statusElement.text(message);
-}
+ function showStatusMessage(type, message) {
+     $('#statusContainer').show();
+     var statusElement = $('#statusContainer h6.' + type);
+     statusElement.text(message);
+ }
